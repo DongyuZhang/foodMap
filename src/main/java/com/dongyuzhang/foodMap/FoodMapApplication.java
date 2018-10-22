3666060d384a50dc54a2f4b36ab8a587f17a8a07
Dongyu Zhang <>
2018-10-22 13:58:15 -0400

    Initial commit

---
+++ src/main/java/com/dongyuzhang/foodMap/FoodMapApplication.java
@@ -0,0 +1,12 @@
+package com.dongyuzhang.foodMap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodMapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodMapApplication.class, args);
+	}
+}
